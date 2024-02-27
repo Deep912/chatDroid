b13983d541fa7de3a140c7104c7cbd8e9eeeaa4c
Deep912 <deepchhegaliya@gmail.com>
2024-02-27 07:12:18 -0800

    added message adapter

--- app/src/main/java/com/example/chatdroid/MainActivity.java
+++ app/src/main/java/com/example/chatdroid/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.chatdroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    TextView txtWelcome;
+    EditText edtMessage;
+    ImageButton btnSend;
+    List<Message> messageList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.chat_rv);
+        txtWelcome = findViewById(R.id.txtWelcome);
+        edtMessage = findViewById(R.id.message_edit_text);
+        btnSend = findViewById(R.id.send_btn);
+
+        messageList = new ArrayList<>();
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String question = edtMessage.getText().toString().trim();
+            }
+        });
     }
 }
